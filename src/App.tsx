0c27cc3d55024dc3ef46b6b88e71f38227f55077
lelegoyeng <lelegoyengs@gmail.com>
2024-01-03 17:05:50 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Sidebar from './components/sidebar';
 import gambartable from './assets/table.png'
 import Modal from './components/modal';
 import { useState } from 'react';
+import produk1 from './assets/produk/burger.png';
+import produk2 from './assets/produk/roti.png';
+import produk3 from './assets/produk/kentang.png';
 
 const tableData = [
   {
@@ -34,29 +37,53 @@ const tableData = [
 
 function App() {
   const [modalOption, setModalOption] = useState({});
+  const [qty, setQty] = useState(0)
 
   const ModalHandle = (data: any) => {
     const handleConfirm = () => {
       console.log(tableData)
     };
 
-
-    console.log(data)
-    document.getElementById('my_modal_1').showModal()
+    document.getElementById('my_modal_1').showModal();
     setModalOption({
       show: true,
       title: `Table ${data?.g?.id}`,
       content: (
         <div>
-          <div>
-            aaaaaaaaaa
+          <hr className='border mt-5 mb-5' />
+          <div className='flex mt-5 mb-5'>
+            <img src={produk1} alt={`produk1`} className='w-16 h-16' />
+            <div className='ml-10 flex justify-center items-center text-2xl'>Rp. 5000</div>
+            <div className='flex justify-center items-center ml-28'>
+              <div className='flex '><button onClick={() => console.log('tambah')} className='btn btn-xs bg-slate-700 text-white'>+</button></div>
+              <div className='text-2xl ml-5 mr-5'>{qty}</div>
+              <div className='flex '><button onClick={() => console.log('kurang')} className='btn btn-xs bg-slate-700 text-white'>-</button></div>
+            </div>
+          </div>
+          <div className='flex mt-5 mb-5'>
+            <img src={produk2} alt={`produk2`} className='w-16 h-16' />
+            <div className='ml-10 flex justify-center items-center text-2xl'>Rp. 15000</div>
+            <div className='flex justify-center items-center ml-28'>
+              <div className='flex '><button onClick={() => console.log('tambah')} className='btn btn-xs bg-slate-700 text-white'>+</button></div>
+              <div className='text-2xl ml-5 mr-5'>{qty}</div>
+              <div className='flex '><button onClick={() => console.log('kurang')} className='btn btn-xs bg-slate-700 text-white'>-</button></div>
+            </div>
+          </div>
+          <div className='flex mt-5 mb-5'>
+            <img src={produk3} alt={`produk3`} className='w-16 h-16' />
+            <div className='ml-10 flex justify-center items-center text-2xl'>Rp. 21000</div>
+            <div className='flex justify-center items-center ml-28'>
+              <div className='flex '><button onClick={() => console.log('tambah')} className='btn btn-xs bg-slate-700 text-white'>+</button></div>
+              <div className='text-2xl ml-5 mr-5'>{qty}</div>
+              <div className='flex '><button onClick={() => console.log('kurang')} className='btn btn-xs bg-slate-700 text-white'>-</button></div>
+            </div>
           </div>
           <div className="modal-action">
             <form method="dialog">
               <button className="btn" onClick={handleConfirm}>Konfirm</button>
             </form>
           </div>
-        </div>
+        </div >
       ),
 
     });
